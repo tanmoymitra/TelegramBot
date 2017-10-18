657f54e017e7b7a3fa53f243816a01a3fa3dd5f8
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-10-18 16:24:42 +0530

    no message

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const url = process.env.APP_URL || 'https://<app-name>.herokuapp.com:443';
 const bot = new TelegramBot(TOKEN, {polling: true});
 
 bot.setWebHook(`${url}/bot${TOKEN}`);
-bot.onText(/\/start/, (msg) => {    
-    bot.sendMessage(msg.chat.id, "Welcome "+msg.chat.first_name);        
-});
+
 bot.on('message', function onMessage(msg) {
     //bot.sendMessage(msg.chat.id, 'Welcome '+msg.chat.first_name+' '+msg.chat.last_name);
     let apiai = apiaiApp.textRequest(msg.text, {
